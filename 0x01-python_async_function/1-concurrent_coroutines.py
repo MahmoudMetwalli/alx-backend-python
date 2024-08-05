7c0f65677daf5a224b920d5f6ddcbcc2da822eca
MahmoudMetwalli <mammkbih@gmail.com>
2024-08-05 23:45:43 +0300

    2024-08-05 11:45:43 PM

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+the wait_n coroutine
+'''
+wait_random = __import__('0-basic_async_syntax').wait_random
+import asyncio
+import random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """Spams wait_random"""
+    list_of_floats = []
+    for i in range(0, n):
+        list_of_floats.append(await wait_random(max_delay))
+    return list_of_floats.sort()
