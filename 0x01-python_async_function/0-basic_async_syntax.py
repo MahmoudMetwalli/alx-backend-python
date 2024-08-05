46483430fa7a71091b982640853a2ffa27a0a9a7
MahmoudMetwalli <mammkbih@gmail.com>
2024-08-05 23:27:50 +0300

    2024-08-05 11:27:49 PM

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Async and await
+"""
+
+import asyncio
+import random
+
+async def wait_random(max_delay=10):
+    """Waiting"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
