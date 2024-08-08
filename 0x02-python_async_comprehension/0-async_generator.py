b76e2f691426fdea684e7e75df4a3e8af48985e9
MahmoudMetwalli <mammkbih@gmail.com>
2024-08-08 03:05:44 +0300

    2024-08-08 03:05:44 AM

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,12 +2,13 @@
 """
 Async Comprehension task 0
 """
+from typing import Generator
 import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Comprehension"""
     for i in range(10):
-        yield random.uniform(1, 10)
         await asyncio.sleep(1)
+        yield random.uniform(1, 10)
